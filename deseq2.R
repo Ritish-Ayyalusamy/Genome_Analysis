ae7d1529f3c23b25c78cc5a7cb392230927f272e
Ritish-Ayyalusamy <69697743+Ritish-Ayyalusamy@users.noreply.github.com>
2022-05-19 23:31:25 +0200

    Add files via upload

---
+++ deseq2.R
@@ -0,0 +1,10 @@
+# DIFFERENTIAL EXPRESSION ANALYSIS
+# Analysis of differential expression and construction of PCA plot
+
+# Load libraries
+library(data.table)
+library(DESeq2)
+library(ggplot2)
+library(EnhancedVolcano)
+library(reshape2)
+library(dplyr)
