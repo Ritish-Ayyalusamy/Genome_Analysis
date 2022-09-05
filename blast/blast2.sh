f6a470cd6eca4aa2ba7e25ddad72db8e92ef6100
pecanpeaches <69697743+pecanpeaches@users.noreply.github.com>
2022-09-06 00:23:46 +0200

    blast file

---
+++ blast/blast2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2022-2-10
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 02:00:00
+#SBATCH -J blastsynteny
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ritish.ayyalusamy.7872@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load blast
+
+# variables
+cd /home/ritish/genomeanalysis/blast/
+
+blastn -query /home/ritish/genomeanalysis/canu/assembled_canu.contigs.fasta \
+ -subject /home/ritish/genomeanalysis/blast/lfml04.fasta -out blastn2.rs  -outfmt 7
+
+
