7c685b6e900ed9f4cbcc0c340e20074a6c889ba6
Ritish-Ayyalusamy <69697743+Ritish-Ayyalusamy@users.noreply.github.com>
2022-09-12 13:36:31 +0200

    Add files via upload

---
+++ blast3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2022-2-10
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 02:00:00
+#SBATCH -J blastsynteny
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ritish.ayyalusamy.7872@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load blast
+
+# variables
+cd /home/ritish/genomeanalysis/blast/
+
+blastn -query /home/ritish/genomeanalysis/canu/assembled_canu.contigs.fasta \
+ -subject /home/ritish/genomeanalysis/blast/lfc2-3.fasta -out blastn3.rs  -outfmt 7
+
